df8971d523fb733083f5f824972b947f86c03bf4
jignom368 <jingom369@gmail.com>
2022-05-14 18:30:33 +0900

    last update

--- landing/models.py
+++ landing/models.py
@@ -21,6 +21,3 @@ class Post(models.Model):
     email = models.EmailField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"title: {self.title}"
